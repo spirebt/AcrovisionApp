d2d729cfef38d55eb962b7345f996f95ef92b98a
spirebt <spirebt@gmail.com>
2012-06-05 11:07:59 +0200

    First Commit

---
+++ PartyApp/PartyAppTests/PartyAppTests.h
@@ -0,0 +1,13 @@
+//
+//  PartyAppTests.h
+//  PartyAppTests
+//
+//  Created by Spire Jankulovski on 6/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PartyAppTests : SenTestCase
+
+@end
---
+++ PartyApp/PartyAppTests/PartyAppTests.m
@@ -0,0 +1,32 @@
+//
+//  PartyAppTests.m
+//  PartyAppTests
+//
+//  Created by Spire Jankulovski on 6/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PartyAppTests.h"
+
+@implementation PartyAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PartyAppTests");
+}
+
+@end
